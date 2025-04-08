d6905a3df99ee0a8af4fddec52ec17f0bf47f658
prasantaaws1 <105577264+prasantaaws1@users.noreply.github.com>
2025-04-09 04:54:34 +0530

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -1,1 +1,17 @@
- 
+provider "aws" {
+  region     = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket          = "demo-20250508"
+    key               = "statefiles/terraform.tfstate"
+    region           = "us-east-1"
+    dynamodb_table = "mytab"
+    encrypt        = true
+  }
+}
+
+module "vpc-infra" {
+  source = "./modules/vpc"
+}

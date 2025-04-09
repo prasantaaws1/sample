c29791ffd3528a45a613114cf08c557a4e8e310f
prasantaaws1 <105577264+prasantaaws1@users.noreply.github.com>
2025-04-09 07:08:49 +0530

    Update main.tf

--- src/modules/vpc/main.tf
+++ src/modules/vpc/main.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "vpc" {
 
 resource "aws_subnet" "sn1-public" {
   count             = var.az_count
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.10.0/24"
   vpc_id                  = aws_vpc.vpc.id
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
@@ -27,7 +27,7 @@ resource "aws_subnet" "sn1-public" {
 
 resource "aws_subnet" "sn2-private" {
   count             = var.az_count
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.20.0/24"
   vpc_id                  = aws_vpc.vpc.id
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true

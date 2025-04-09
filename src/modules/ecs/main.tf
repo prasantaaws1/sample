b48c82a218b69a06d05cf37ce3f196ef1ebff3e2
prasantaaws1 <105577264+prasantaaws1@users.noreply.github.com>
2025-04-09 05:42:32 +0530

    Update main.tf

--- src/modules/ecs/main.tf
+++ src/modules/ecs/main.tf
@@ -16,7 +16,7 @@ resource "aws_ecs_service" "service" {
   network_configuration {
     assign_public_ip = true
     security_groups  = aws_security_group.sg.id
-    subnets          = aws_subnet.sn1-public.id, aws_subnet.sn2-public.id
+    subnets          = [aws_subnet.sn1-public.id, aws_subnet.sn2-public.id]
   }
 }
 
